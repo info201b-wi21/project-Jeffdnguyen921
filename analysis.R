d818bdf266463aaec14812fdcd07d6376e39271f
Jeffrey Nguyen <nguyej21@uw.edu>
2021-02-21 23:32:56 -0800

    Loaded data in the analysis.R file

--- analysis.R
+++ analysis.R
@@ -1,1 +1,6 @@
-This is gonna be our main project!
+
+library(readr)
+SSDB_Raw_Data_df <- read_csv("C:/Users/nguye/OneDrive/Desktop/INFO_201_Project/project-Jeffdnguyen921/data/SSDB_Raw_Data.csv")
+View(SSDB_Raw_Data_df)
+Unemployment_df <- read_csv("C:/Users/nguye/OneDrive/Desktop/INFO_201_Project/project-Jeffdnguyen921/data/Unemployment.csv")
+View(Unemployment_df) 
