389666305ca341cbe6722f1de802e78d9fb32100
ElizaRMoore <mooree6@uw.edu>
2021-02-23 14:36:47 -0800

    read csv fix

--- analysis.R
+++ analysis.R
@@ -1,6 +1,6 @@
 
 library(readr)
-SSDB_Raw_Data_df <- read_csv("C:/Users/nguye/OneDrive/Desktop/INFO_201_Project/project-Jeffdnguyen921/data/SSDB_Raw_Data.csv")
+SSDB_Raw_Data_df <- read_csv("https://raw.githubusercontent.com/info201b-wi21/project-Jeffdnguyen921/main/data/SSDB_Raw_Data.csv?token=ASMBDXHD5IM7WB37PKUT2FTAH24JC")
 View(SSDB_Raw_Data_df)
-Unemployment_df <- read_csv("C:/Users/nguye/OneDrive/Desktop/INFO_201_Project/project-Jeffdnguyen921/data/Unemployment.csv")
+Unemployment_df <- read_csv("https://raw.githubusercontent.com/info201b-wi21/project-Jeffdnguyen921/main/data/Unemployment.csv?token=ASMBDXGZ36J2NRSGNKLLWDLAH24MU")
 View(Unemployment_df) 
